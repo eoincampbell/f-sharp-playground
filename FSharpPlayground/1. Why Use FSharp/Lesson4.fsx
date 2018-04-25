3ccd34a81298feb52d8d661059cb79793692910f
Eoin Campbell <eoin.campbell@gmail.com>
2018-04-25 07:23:46 +0100

    Lessons 4 & 5

---
+++ FSharpPlayground/1. Why Use FSharp/Lesson4.fsx
@@ -0,0 +1,31 @@
+﻿let rec quicksort list =
+    match list with
+    | [] -> []                         
+    | first::remainder ->              
+        let a = remainder |> List.filter (fun e->e < first) 
+        let z = remainder |> List.filter (fun e->e >= first) 
+        List.concat [quicksort a; [first]; quicksort z]
+
+let ans = quicksort [1;5;23;18;9;1;3]
+
+
+    
+let rec quicksort2 list = 
+    match list with
+    | [] -> []                         
+    | first::remainder ->              
+        let a,z = List.partition ((>=) first) remainder
+        List.concat [quicksort2 a; [first]; quicksort2 z]
+
+let ans2 = quicksort2 [1;5;23;18;9;1;3]
+
+let rec quicksort3 = function
+    | [] -> []                         
+    | first::remainder ->              
+        let a,z = List.partition ((>=) first) remainder
+        List.concat [quicksort3 a; [first]; quicksort3 z]
+
+let ans3 = quicksort3 [1;5;23;18;9;1;3]
+
+ans2
+ans3
---
+++ FSharpPlayground/1. Why Use FSharp/Lesson5.fsx
@@ -0,0 +1,20 @@
+﻿open System.Net
+open System
+open System.IO
+
+let fetchUrl callback url = 
+    let req = WebRequest.Create(Uri(url))
+    use res = req.GetResponse()
+    use stream = res.GetResponseStream()
+    use reader = new StreamReader(stream)
+    callback reader url
+
+let myCallback (reader:StreamReader) url = 
+    let html = reader.ReadToEnd()
+    let html100 = html.Substring(0,100)
+    printfn "Downloaded %s. First 100 is %s" url html100
+
+
+//let google = fetchUrl myCallback "http://www.google.com"
+
+//let fetch
